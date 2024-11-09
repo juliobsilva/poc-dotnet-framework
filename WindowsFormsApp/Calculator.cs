1e8df2e9e73cb49bad39d5a5758f98a7da389d2e
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-11-08 23:51:58 -0300

    Add teste

---
+++ WindowsFormsApp/Calculator.cs
@@ -0,0 +1,11 @@
+// WindowsFormsApp/Calculator.cs
+namespace WindowsFormsApp
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
